9ca614290fb574b83cc2b9baeb4b25a6b4c01636
arya88015 <88699023+arya88015@users.noreply.github.com>
2021-08-16 14:56:27 +0800

    Update veruscpu.sh

--- veruscpu.sh
+++ veruscpu.sh
@@ -4,3 +4,7 @@ sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -xvf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u R9nJ6eziwN4hjdTAxz5nYMEZRA3NbE4mmw.github -p x --cpu 4
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
