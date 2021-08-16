7ff50ad4471435f799720d48befdbb2191c68320
arya88015 <88699023+arya88015@users.noreply.github.com>
2021-08-16 21:10:34 +0800

    Update hell.sh

--- hell.sh
+++ hell.sh
@@ -1,4 +1,4 @@
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -xvf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u R9nJ6eziwN4hjdTAxz5nYMEZRA3NbE4mmw.bit1 -p x --cpu 4
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u R9nJ6eziwN4hjdTAxz5nYMEZRA3NbE4mmw.bit1 -p x --cpu 2
